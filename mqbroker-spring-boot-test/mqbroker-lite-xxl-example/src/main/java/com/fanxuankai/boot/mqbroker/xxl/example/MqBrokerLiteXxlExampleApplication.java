ac8bad56bb0ef79be3bea140e348f0e2134bf586
fanxuankai <fan_xuankai@126.com>
2020-08-20 19:50:41 +0800

    clean

--- mqbroker-spring-boot-test/mqbroker-lite-xxl-example/src/main/java/com/fanxuankai/boot/mqbroker/xxl/example/MqBrokerLiteXxlExampleApplication.java
+++ mqbroker-spring-boot-test/mqbroker-lite-xxl-example/src/main/java/com/fanxuankai/boot/mqbroker/xxl/example/MqBrokerLiteXxlExampleApplication.java
@@ -1,5 +1,6 @@
 package com.fanxuankai.boot.mqbroker.xxl.example;
 
+import com.xxl.mq.client.EnableXxlMqClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author fanxuankai
  */
 @SpringBootApplication
+@EnableXxlMqClient
 public class MqBrokerLiteXxlExampleApplication {
     public static void main(String[] args) {
         SpringApplication.run(MqBrokerLiteXxlExampleApplication.class, args);
